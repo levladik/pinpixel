b2627d67e29b192c6318077d36718a0d3dc0cb70
Levladik <vladislavtyshenko@yandex.ru>
2025-01-25 16:22:06 +0400

    feat: enhance CustomSearchBox to handle undefined mapRef and improve country retrieval

--- src/components/CustomizationPanel/CustomSearchBox.tsx
+++ src/components/CustomizationPanel/CustomSearchBox.tsx
@@ -4,37 +4,35 @@ import { usePosterStore } from "../../store/map.poster-store";
 import { SearchBox } from '@mapbox/search-js-react';
 import { mapboxConfig } from "../../config/mapboxConfig";
 import { useState } from "react";
+import { SearchBoxRetrieveResponse } from '@mapbox/search-js-core';
 
 export const CustomSearchBox = () => {
 
   const [inputValue, setInputValue] = useState("");
   const { mapRef } = UseMapboxStore();
-  const { city, setCity, country, setCountry, coordinates, setCoordinates } = usePosterStore();
+  const { setCity, setCountry, setCoordinates } = usePosterStore();
 
-  const handleRetrieve = (result) => {
+  const handleRetrieve = (result: SearchBoxRetrieveResponse ) => {
     setCity(result.features[0].properties.name);
-    setCountry(result.features[0].properties?.context?.country?.name);
+    setCountry(result.features[0].properties.context?.country?.name || "");
     setCoordinates(result.features[0].geometry.coordinates);
-    console.log(city, country, coordinates);    
   };
 
   return (
-    <div>
-    <SearchBox
-      accessToken={mapboxConfig.accessToken}
-      map={mapRef}
-      mapboxgl={mapboxgl}
-      options={{
-        types: 'place',
-        zoom: 12
-      }}
-      placeholder="Seacr your city"
-      value={inputValue}
-      onChange={(input) => {
-        setInputValue(input);
-      }}
-      onRetrieve={handleRetrieve  }
-    />
-    </div>
+      // @ts-expect-error: mapRef might be null
+      <SearchBox
+        accessToken={mapboxConfig.accessToken}
+        map={mapRef || undefined}
+        mapboxgl={mapboxgl}
+        options={{
+          types: 'place',
+        }}
+        placeholder="Seacr your city"
+        value={inputValue}
+        onChange={(input) => {
+          setInputValue(input);
+        }}
+        onRetrieve={handleRetrieve}
+      />
   );
 };
