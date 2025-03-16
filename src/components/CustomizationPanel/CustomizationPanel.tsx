6e75e8e3d62cfa43326cacf0bf23457b39c893d6
Levladik <vladislavtyshenko@yandex.ru>
2025-03-16 11:56:12 +0400

    refactor: update import statements for consistency in CustomizationPanel component

--- src/components/CustomizationPanel/CustomizationPanel.tsx
+++ src/components/CustomizationPanel/CustomizationPanel.tsx
@@ -1,18 +1,15 @@
-import { MapSizeButtons } from "./MapSizeButtons";
-import { CustomSearchBox } from "./CustomSearchBox";
-import { UserLabels } from "./UserLabels";
-import { MapStyles } from "./MapStyles";
+import { MapSizeButtons } from './MapSizeButtons/MapSizeButtons';
+import { CustomSearchBox } from './CustomSearchBox';
+import { UserLabels } from './UserLabels';
+import { MapStyles } from './MapStyles';
 
 export const CustomizationPanel = () => {
-
   return (
-    <div className="customisation-panel__wrapper col-12 col-md-4 order-2 order-md-1 p-3 d-grid gap-1">
-      
+    <div className='customisation-panel__wrapper col-12 col-md-4 order-2 order-md-1 p-3 d-grid gap-1'>
       <CustomSearchBox />
       <UserLabels />
       <MapStyles />
       <MapSizeButtons />
-      
     </div>
   );
 };
